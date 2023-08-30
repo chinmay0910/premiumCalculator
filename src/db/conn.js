7079bb4e49d8b63eec8f5452a57b88b88056f4a2
chinmay0910 <chinmayrmhatre@gmail.com>
2023-08-30 22:47:34 +0530

    Error Fixes

--- src/db/conn.js
+++ src/db/conn.js
@@ -3,3 +3,5 @@ const mongoose = require('mongoose');
 // mongoose.connect("mongodb://0.0.0.0:27017/chinmay").then(console.log("Connected to server"))
 mongoose.connect("mongodb+srv://chinmayrmhatre:ZUNJtG8ZxFADZU9E@premiumcalculator.uelz72i.mongodb.net/?retryWrites=true&w=majority").then(console.log("Connected to server"))
 .catch((e)=> console.log(`Not Connected -- ${e}`));
+
+//115.96.218.93/32 
