74cee63faa1d6de7707a43ed7d6987e0c1f203c6
chinmay0910 <chinmayrmhatre@gmail.com>
2023-09-03 18:57:53 +0530

    Errors

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
+let mongoDB_URL = process.env.mongoDB_URL;
 // mongoose.connect("mongodb://0.0.0.0:27017/chinmay").then(console.log("Connected to server"))
-mongoose.connect("mongodb+srv://chinmayrmhatre:ZUNJtG8ZxFADZU9E@premiumcalculator.uelz72i.mongodb.net/?retryWrites=true&w=majority").then(console.log("Connected to server"))
+mongoose.connect(mongoDB_URL).then(console.log("Connected to server"))
 .catch((e)=> console.log(`Not Connected -- ${e}`));
 
 //115.96.218.93/32 
